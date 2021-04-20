dab21a24e01b603ae78e302de8aa625ca09a60c6
unknown <abhishek.garg_cs16@gla.ac.in>
2021-04-20 13:51:08 +0530

    Scanner ex

---
+++ exe.java
@@ -0,0 +1,10 @@
+public class ScannerEx {
+	static Scanner sc = new Scanner(System.in);
+	public static void main(String branch[]){
+		
+		System.out.println("Which is Your Branch");
+		String st = sc.next();
+		System.out.println("My Branch Is : "+st);
+	}
+}
+
