690e65c9455fef1c88f699d0aa036200bfc5e816
Abhishek Garg <39979216+abhishekcs16@users.noreply.github.com>
2021-04-21 14:19:50 +0530

    Remove Scanner

--- exe.java
+++ exe.java
@@ -1,10 +1,9 @@
 public class ScannerEx {
-	static Scanner sc = new Scanner(System.in);
+	
 	public static void main(String branch[]){
 		
 		System.out.println("Which is Your Branch");
-		String st = sc.next();
-		System.out.println("My Branch Is this : "+st);
+		
 	}
 }
 
