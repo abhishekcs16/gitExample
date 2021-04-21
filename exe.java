63819b8ad086b5d4e2d5a99b18e3245895c9b4cc
Abhishek Garg <39979216+abhishekcs16@users.noreply.github.com>
2021-04-21 14:25:40 +0530

    Added

--- exe.java
+++ exe.java
@@ -1,9 +1,11 @@
 public class exe {
 	
+	
+	static Scanner sc = new Scanner(System.in);
 	public static void main(String branch[]){
 		
 		System.out.println("Which is Your Branch");
-		
+		String st = sc.next();
+		System.out.println("My Branch Is this : "+st);
 	}
 }
-
